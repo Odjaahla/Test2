0cba19d84ce1299227f2ddc05d9862b1424bdd07
Ojaahla <rikardojala@gmail.com>
2016-01-30 08:40:21 +0100

    Tjoho

--- src/com/fiskftw/Main.java
+++ src/com/fiskftw/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World again");
+        System.out.println("Hello World yet again");
 
         // write your code here
     }
